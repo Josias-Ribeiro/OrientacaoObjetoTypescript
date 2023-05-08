8e35eef0d11793468bc8f48e6b9373eacffb0ff8
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2023-05-07 22:28:54 -0300

    Ajustando teste unitário Contato.ts
    Iniciando o arquivo de teste unitário da entidade contato.

--- src/model/entity/Contato.ts
+++ src/model/entity/Contato.ts
@@ -1,63 +1,66 @@
 import { validate } from 'email-validator'
 
 export class Contato {
-  private _nome: string;
-  private _telefone: string;
-  private _email: string;
-  private _dataNascimento: Date;
 
-  constructor(
-    nome: string,
-    telefone: string,
-    email: string,
-    dataNascimento: Date
-  ) {
-    this.nome = nome
-    this.telefone = telefone
-    this.email = email
-    this.dataNascimento = dataNascimento
-  }
+    private _nome: string
+    private _telefone: string
+    private _email: string
+    private _dataNascimento: Date
 
-  get nome(): string {
-    return this._nome;
-  }
-
-  set nome(nome: string) {
-    if (nome && nome.length > 0) this._nome = nome;
-    else {
-      console.log("nome inválido");
+    constructor(nome: string, telefone: string, email: string, dataNascimento: Date) {
+        this.nome = nome
+        this.telefone = telefone
+        this.email = email
+        this.dataNascimento = dataNascimento
     }
-  }
 
-  get telefone(): string {
-    return this._telefone;
-  }
-
-  set telefone(telefone: string) {
-    const regex = /^(\+\d{2}.)?\(\d{2}\).(\d{5}|\d{4})-\d{4}$/;
-    const validador = new RegExp(regex);
-
-    if (telefone && validador.test(telefone)) this._telefone = telefone;
-    else {
-      console.log("telefone inválido");
+    get nome(): string {
+        return this._nome
     }
-  }
 
-  get email(): string{
-    return this._email
-  }
+    set nome(nome: string) {
+        if (nome && nome.length > 0) {
+            this._nome = nome
+        } else {
+            console.log('Nome é inválido')
+        }
+    }
 
-  set email(email: string){
-    if (email && validate(email)) this.email = email
-    else {console.log('email inválido')}
-  }
+    get telefone(): string {
+        return this._telefone
+    }
 
-  get dataNascimento(): Date {
-    return this._dataNascimento;
-  }
+    set telefone(telefone: string) {
+        const regex = /^(\+\d{2}.)?\(\d{2}\).(\d{5}|\d{4})-\d{4}$/
+        const validador = new RegExp(regex)
+        if (telefone && validador.test(telefone)) {
+            this._telefone = telefone
+        } else {
+            console.log('Telefone inválido')
+        }
+    }
 
-  set dataNascimento(dataNascimento: Date) {
-    if (dataNascimento && dataNascimento < new Date()) this._dataNascimento = dataNascimento
-    else {console.log('data de nascimento inválida')}
-  }
+    get email(): string {
+        return this._email
+    }
+
+    set email(email: string) {
+        if (email && validate(email)) {
+            this._email = email
+        } else {
+            console.log('E-mail inválido')
+        }
+    }
+
+    get dataNascimento(): Date {
+        return this._dataNascimento
+    }
+
+    set dataNascimento(dataNascimento: Date) {
+        if (dataNascimento && dataNascimento < new Date()) {
+            this._dataNascimento = dataNascimento
+        } else {
+            console.log('Data de nascimento inválida')
+        }
+    }
 }
